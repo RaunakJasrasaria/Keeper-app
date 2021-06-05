32b357c67fe899d213185294b97eab0d8ef8a438
raunak jasrasaria <raunakjasrasaria04@gmail.com>
2021-06-05 19:42:20 +0530

    base commit

---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+      <h1>heading for note</h1>
+      <p>content for note</p>
+    </div>
+  );
+}
+
+export default Note;
