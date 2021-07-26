efa01ce0c4930e7eabddba029d691de7bd54a816
raunak jasrasaria <raunakjasrasaria04@gmail.com>
2021-07-27 00:42:31 +0530

    deploy

---
+++ build/precache-manifest.81edc0a183d28497670d7bd484aa2e14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c2da736f8144302aef015be3253ac1",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "5ff1be8904f718d58787",
+    "url": "/Keeper-app/static/js/2.8ea6025c.chunk.js"
+  },
+  {
+    "revision": "7e3e65114a8783c1143c",
+    "url": "/Keeper-app/static/js/main.4e2b2e74.chunk.js"
+  },
+  {
+    "revision": "33cb2174e8328f4b935a",
+    "url": "/Keeper-app/static/js/runtime-main.b2f67e31.js"
+  }
+]);
